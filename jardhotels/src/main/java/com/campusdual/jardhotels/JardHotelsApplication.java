18a984fdd4a82f363b3fb598e80feb97928df201
Antonio Crespo <antoniocrespgome@gmail.com>
2023-05-15 12:41:28 +0200

    adding proyect

---
+++ jardhotels/src/main/java/com/campusdual/jardhotels/JardHotelsApplication.java
@@ -0,0 +1,13 @@
+package com.campusdual.jardhotels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JardHotelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JardHotelsApplication.class, args);
+	}
+
+}
---
+++ jardhotels/src/test/java/com/campusdual/jardhotels/JardHotelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusdual.jardhotels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JardHotelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

1ec87a996b6527ae2bd418ded61db3a0af7b8b05
Antonio Crespo <antoniocrespgome@gmail.com>
2023-05-15 14:39:00 +0200

    adding some classes to model

--- jardhotels/src/main/java/com/campusdual/jardhotels/service/HotelService.java
+++ jardhotels/src/main/java/com/campusdual/jardhotels/service/HotelService.java
@@ -6,7 +6,11 @@ import com.campusdual.jardhotels.model.dao.HotelDAO;
 import com.campusdual.jardhotels.model.dto.HotelDTO;
 import com.campusdual.jardhotels.model.dto.dtomapper.HotelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
 
+@Service("HotelService")
+@Lazy
 public class HotelService implements IHotelService {
 
     @Autowired

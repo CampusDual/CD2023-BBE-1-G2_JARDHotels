25c3b14dd46ddd70a1d63419f9f1ba0f66592682
Lunantisidhe <rebeca.gonzalez.balado@gmail.com>
2023-05-19 12:33:02 +0200

    limpieza test controlador

--- jardhotels/src/test/java/com/campusdual/jardhotels/controller/HotelControllerTest.java
+++ jardhotels/src/test/java/com/campusdual/jardhotels/controller/HotelControllerTest.java
@@ -1,109 +1,114 @@
 package com.campusdual.jardhotels.controller;
 
-
 import com.campusdual.jardhotels.exceptions.HotelNotFound;
 import com.campusdual.jardhotels.model.Hotel;
 import com.campusdual.jardhotels.model.dto.HotelDTO;
 import com.campusdual.jardhotels.service.HotelService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class HotelControllerTest {
+
     private MockMvc mockMvc;
+
     @InjectMocks
     HotelController hotelController;
+
     @Mock
     HotelService hotelService;
+
     @BeforeEach
-    void setup(){
+    void setup() {
         mockMvc = MockMvcBuilders.standaloneSetup(hotelController).build();
     }
+
     @Test
     void addHotelTest() throws Exception {
+
+        // given
         Hotel hotel = new Hotel();
-        hotel.setName("hotelname");
-        hotel.setStars(1);
-        hotel.setAddress("address");
-        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/hotels/add").contentType(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsString(hotel))).andReturn();
-        assertEquals(HttpStatus.OK.value(),mvcResult.getResponse().getStatus());
+
+        // when
+        MvcResult addHotelMvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/hotels/add")
+                .contentType(MediaType.APPLICATION_JSON).content(new ObjectMapper().writeValueAsString(hotel))).andReturn();
+
+        // then
+        assertEquals(HttpStatus.OK.value(), addHotelMvcResult.getResponse().getStatus());
     }
+
+    @Test
+    void getAllHotelTest() throws Exception {
+
+        // given
+        List<HotelDTO> hotelDTOS = List.of(new HotelDTO(), new HotelDTO());
+
+        // when
+        when(hotelService.queryAll()).thenReturn(hotelDTOS);
+        MvcResult getAllMvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/hotels/getAll")).andReturn();
+
+        // then
+        verify(hotelService, times(1)).queryAll();
+        assertEquals(HttpStatus.OK.value(), getAllMvcResult.getResponse().getStatus());
+        assertEquals(hotelDTOS.size(), new ObjectMapper().readTree(getAllMvcResult.getResponse()
+                .getContentAsString()).size());
+
+    }
+
     @Test
     void deleteHotelTest() throws Exception {
+
+        // given
         HotelDTO hotelDTO = new HotelDTO();
         hotelDTO.setId(1);
-        hotelDTO.setName("Hotel");
-        hotelDTO.setStars(5);
-        hotelDTO.setAddress("Dirección");
+
+        // when
         when(hotelService.queryHotel(any(HotelDTO.class))).thenReturn(hotelDTO);
         when(hotelService.deleteHotel(any(HotelDTO.class))).thenReturn(1);
+
         MvcResult deleteMvcResult = mockMvc.perform(MockMvcRequestBuilders.delete("/hotels/delete")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(hotelDTO)))
-                .andReturn();
-        assertEquals(HttpStatus.OK.value(), deleteMvcResult.getResponse().getStatus());
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(hotelDTO))).andReturn();
+
+        // then
         verify(hotelService, times(1)).queryHotel(any(HotelDTO.class));
         verify(hotelService, times(1)).deleteHotel(any(HotelDTO.class));
+        assertEquals(HttpStatus.OK.value(), deleteMvcResult.getResponse().getStatus());
     }
+
     @Test
-    void deleteNotExistingHotelTest() throws Exception{
+    void deleteNotExistingHotelTest() throws Exception {
+
+        // given
         HotelDTO hotelDTO = new HotelDTO();
+
+        // when
         when(hotelService.queryHotel(any(HotelDTO.class))).thenThrow(new HotelNotFound("Hotel not found"));
         MvcResult deleteMvcResult = mockMvc.perform(MockMvcRequestBuilders.delete("/hotels/delete")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(hotelDTO)))
-                .andReturn();
+                        .content(new ObjectMapper().writeValueAsString(hotelDTO))).andReturn();
+
+        // then
         assertEquals("Hotel not found",deleteMvcResult.getResponse().getErrorMessage());
-    }
-
-    @Test
-    void getAllHotelTest() throws Exception{
-        List<HotelDTO> hotelDTOS = new ArrayList<HotelDTO>();
-        HotelDTO hotel1 = new HotelDTO();
-        hotel1.setId(1);
-        hotel1.setName("Hotel");
-        hotel1.setStars(5);
-        hotel1.setAddress("Dirección");
-        HotelDTO hotel2 = new HotelDTO();
-        hotel2.setId(2);
-        hotel2.setName("Hotel2");
-        hotel2.setStars(3);
-        hotel2.setAddress("Dirección2");
-        hotelDTOS.add(hotel1);
-        hotelDTOS.add(hotel2);
-        when(hotelService.queryAll()).thenReturn(hotelDTOS);
-        MvcResult getAllMvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/hotels/getAll")).andReturn();
-        assertEquals(HttpStatus.OK.value(), getAllMvcResult.getResponse().getStatus());
-        verify(hotelService, times(1)).queryAll();
-        assertEquals(hotelDTOS.size(),new ObjectMapper().readTree(getAllMvcResult.getResponse().getContentAsString()).size());
-
     }
 }
 
--- jardhotels/src/test/java/com/campusdual/jardhotels/controller/RoomControllerTest.java
+++ jardhotels/src/test/java/com/campusdual/jardhotels/controller/RoomControllerTest.java
@@ -1,14 +1,9 @@
 package com.campusdual.jardhotels.controller;
 
-
 import com.campusdual.jardhotels.exceptions.HotelNotFound;
 import com.campusdual.jardhotels.exceptions.RoomNotFound;
-import com.campusdual.jardhotels.model.Hotel;
-import com.campusdual.jardhotels.model.Room;
 import com.campusdual.jardhotels.model.dto.HotelDTO;
 import com.campusdual.jardhotels.model.dto.RoomDTO;
-import com.campusdual.jardhotels.model.dto.dtomapper.HotelMapper;
-import com.campusdual.jardhotels.model.dto.dtomapper.RoomMapper;
 import com.campusdual.jardhotels.service.HotelService;
 import com.campusdual.jardhotels.service.RoomService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -26,7 +21,6 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -36,111 +30,111 @@ import static org.mockito.Mockito.*;
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class RoomControllerTest {
+
     private MockMvc mockMvc;
+
     @InjectMocks
     RoomController roomController;
+
     @Mock
     HotelService hotelService;
+
     @Mock
     RoomService roomService;
+
     @BeforeEach
-    void setup(){
+    void setup() {
         mockMvc = MockMvcBuilders.standaloneSetup(roomController).build();
     }
+
     @Test
     void addRoomTest() throws Exception {
 
-        Hotel hotel = new Hotel();
-        hotel.setId(1);
-        Room room = new Room();
-        room.setNumber(20);
-        room.setCapacity(1);
-        room.setDescription("descripcion");
-        room.setHotel(hotel);
-        RoomDTO roomDTO = RoomMapper.INSTANCE.toDto(room);
-        HotelDTO hotelDTO = HotelMapper.INSTANCE.toDto(hotel);
+        // given
+        HotelDTO hotelDTO = new HotelDTO();
+        hotelDTO.setId(1);
+        RoomDTO roomDTO = new RoomDTO();
+        roomDTO.setHotel(1);
 
+        // when
         when(hotelService.queryHotel(any(HotelDTO.class))).thenReturn(hotelDTO);
         when(roomService.insertRoom(any(RoomDTO.class))).thenReturn(1);
         MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/rooms/add")
-                .contentType(MediaType.APPLICATION_JSON).content(new ObjectMapper().writeValueAsString(roomDTO))).andReturn();
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(roomDTO))).andReturn();
+
+        // then
         assertEquals(HttpStatus.OK.value(),mvcResult.getResponse().getStatus());
     }
+
     @Test
     void addRoomToNonExistingHotelTest() throws Exception {
 
-        Hotel hotel = new Hotel();
-        hotel.setId(1);
-        Room room = new Room();
-        room.setNumber(20);
-        room.setCapacity(1);
-        room.setDescription("descripcion");
-        room.setHotel(hotel);
-        RoomDTO roomDTO = RoomMapper.INSTANCE.toDto(room);
-        HotelDTO hotelDTO = HotelMapper.INSTANCE.toDto(hotel);
+        // given
+        RoomDTO roomDTO = new RoomDTO();
+        roomDTO.setHotel(99);
 
+        // when
         when(hotelService.queryHotel(any(HotelDTO.class))).thenThrow(new HotelNotFound("Hotel not found"));
         MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/rooms/add")
-                .contentType(MediaType.APPLICATION_JSON).content(new ObjectMapper().writeValueAsString(roomDTO))).andReturn();
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(roomDTO))).andReturn();
+
+        // then
         assertEquals("Hotel not found",mvcResult.getResponse().getErrorMessage());
-    }
-    @Test
-    void deleteRoomTest() throws Exception {
-
-        RoomDTO roomDTO = new RoomDTO();
-        roomDTO.setId(1);
-        roomDTO.setNumber(20);
-        roomDTO.setCapacity(1);
-        roomDTO.setDescription("descripcion");
-        roomDTO.setHotel(1);
-
-        when(roomService.queryRoom(any(RoomDTO.class))).thenReturn(roomDTO);
-        when(roomService.deleteRoom(any(RoomDTO.class))).thenReturn(1);
-        MvcResult deleteMvcResult = mockMvc.perform(MockMvcRequestBuilders.delete("/rooms/delete")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(roomDTO)))
-                .andReturn();
-        assertEquals(HttpStatus.OK.value(), deleteMvcResult.getResponse().getStatus());
-        verify(roomService, times(1)).queryRoom(any(RoomDTO.class));
-        verify(roomService, times(1)).deleteRoom(any(RoomDTO.class));
-    }
-    @Test
-    void deleteNotExistingRoomTest() throws Exception {
-
-        RoomDTO roomDTO = new RoomDTO();
-        when(roomService.queryRoom(any(RoomDTO.class))).thenThrow(new RoomNotFound("Room not found"));
-        MvcResult deleteMvcResult = mockMvc.perform(MockMvcRequestBuilders.delete("/rooms/delete")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(roomDTO)))
-                .andReturn();
-        assertEquals("Room not found",deleteMvcResult.getResponse().getErrorMessage());
     }
 
     @Test
     void getAllRoomTest() throws Exception{
 
-        List<RoomDTO> roomDTOS = new ArrayList<RoomDTO>();
-        RoomDTO room1 = new RoomDTO();
-        room1.setId(1);
-        room1.setNumber(20);
-        room1.setCapacity(1);
-        room1.setDescription("descripcion");
-        room1.setHotel(1);
-        RoomDTO room2 = new RoomDTO();
-        room2.setId(2);
-        room2.setNumber(22);
-        room2.setCapacity(2);
-        room2.setDescription("descripcion");
-        room2.setHotel(1);
-        roomDTOS.add(room1);
-        roomDTOS.add(room2);
+        // given
+        List<RoomDTO> roomDTOS = List.of(new RoomDTO(), new RoomDTO());
 
+        // when
         when(roomService.queryAll()).thenReturn(roomDTOS);
         MvcResult getAllMvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/rooms/getAll")).andReturn();
-        assertEquals(HttpStatus.OK.value(), getAllMvcResult.getResponse().getStatus());
+
+        // then
         verify(roomService, times(1)).queryAll();
+        assertEquals(HttpStatus.OK.value(), getAllMvcResult.getResponse().getStatus());
         assertEquals(roomDTOS.size(),new ObjectMapper().readTree(getAllMvcResult.getResponse().getContentAsString()).size());
 
+    }
+
+    @Test
+    void deleteRoomTest() throws Exception {
+
+        // given
+        RoomDTO roomDTO = new RoomDTO();
+        roomDTO.setId(1);
+
+        // when
+        when(roomService.queryRoom(any(RoomDTO.class))).thenReturn(roomDTO);
+        when(roomService.deleteRoom(any(RoomDTO.class))).thenReturn(1);
+        MvcResult deleteMvcResult = mockMvc.perform(MockMvcRequestBuilders.delete("/rooms/delete")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(roomDTO))).andReturn();
+
+        // then
+        verify(roomService, times(1)).queryRoom(any(RoomDTO.class));
+        verify(roomService, times(1)).deleteRoom(any(RoomDTO.class));
+        assertEquals(HttpStatus.OK.value(), deleteMvcResult.getResponse().getStatus());
+    }
+
+    @Test
+    void deleteNotExistingRoomTest() throws Exception {
+
+        // given
+        RoomDTO roomDTO = new RoomDTO();
+
+        // when
+        when(roomService.queryRoom(any(RoomDTO.class))).thenThrow(new RoomNotFound("Room not found"));
+        MvcResult deleteMvcResult = mockMvc.perform(MockMvcRequestBuilders.delete("/rooms/delete")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(roomDTO))).andReturn();
+
+        // then
+        assertEquals("Room not found",deleteMvcResult.getResponse().getErrorMessage());
     }
 }
 

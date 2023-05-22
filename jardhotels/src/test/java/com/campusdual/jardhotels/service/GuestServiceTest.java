acb067a078dbb17fd19409592c32934d07fa3eb7
juangarciidev <juangarciadam@gmail.com>
2023-05-22 13:02:56 +0200

    adding addGuestTest

---
+++ jardhotels/src/test/java/com/campusdual/jardhotels/service/GuestServiceTest.java
@@ -0,0 +1,45 @@
+package com.campusdual.jardhotels.service;
+
+import com.campusdual.jardhotels.model.Guest;
+import com.campusdual.jardhotels.model.dao.GuestDAO;
+import com.campusdual.jardhotels.model.dto.GuestDTO;
+import com.campusdual.jardhotels.model.dto.dtomapper.GuestMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+
+public class GuestServiceTest {
+    @Mock
+    GuestDAO guestDAO;
+
+    @InjectMocks
+    GuestService guestService;
+
+    @Test
+    void addGuestTest() {
+
+        // given
+        Guest guest = new Guest();
+        guest.setId(1);
+        GuestDTO guestDTO = GuestMapper.INSTANCE.toDto(guest);
+
+        // when
+        when(guestDAO.saveAndFlush(any(Guest.class))).thenReturn(guest);
+        int insertedId = guestService.insertGuest(guestDTO);
+
+        // then
+        verify(guestDAO, times(1)).saveAndFlush(any(Guest.class));
+        assertEquals(guest.getId(), insertedId);
+    }
+}

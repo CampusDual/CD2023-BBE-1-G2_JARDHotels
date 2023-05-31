c96a18644c80fc0e0e47d55c40c0728872b7523f
Juan García <juangarciadam@gmail.com>
2023-05-31 11:49:09 +0200

    Fixing validations

--- jardhotelsontimize/jardhotelsontimize-model/src/main/java/com/campusdual/jardhotelsontimize/model/core/service/RoomService.java
+++ jardhotelsontimize/jardhotelsontimize-model/src/main/java/com/campusdual/jardhotelsontimize/model/core/service/RoomService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.Keymap;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -28,32 +29,29 @@ public class RoomService implements IRoomService {
     @Override
     public EntityResult roomQuery(Map<String, Object> keyMap, List<String> attrList) {
         EntityResult result = this.daoHelper.query(this.roomDao, keyMap, attrList);
-
-        if(result.toString().contains("id"))
+        if (result.toString().contains("id") || result.toString().contains("ID"))
             result.setMessage("The room has been found");
-        else{
+        else {
             result.setMessage("The room doesn't exists");
             result.setColumnSQLTypes(new HashMap());
         }
-
-
         return result;
     }
 
     @Override
     public EntityResult roomInsert(Map<String, Object> attrMap) {
-        List<String>attrList = new ArrayList<>();
+        List<String> attrList = new ArrayList<>();
         attrList.add("id");
         EntityResult result = roomQuery(attrMap, attrList);
         try {
-            result =  this.daoHelper.insert(this.roomDao, attrMap);
+            result = this.daoHelper.insert(this.roomDao, attrMap);
             result.setMessage("Successful room insertion");
-        }catch (Exception e){
-            if(e.getMessage().contains("room_hotel_fkey"))
+        } catch (Exception e) {
+            if (e.getMessage().contains("room_hotel_fkey"))
                 result.setMessage("Hotel not found");
-            else if(e.getMessage().contains("Repeated number in hotel"))
+            else if (e.getMessage().contains("Repeated number in hotel"))
                 result.setMessage("Repeated number in hotel");
-            else if(e.getMessage().contains("Number must be over zero"))
+            else if (e.getMessage().contains("Number must be over zero"))
                 result.setMessage("Number must be over zero");
             else
                 result.setMessage("Capacity must be over zero");
@@ -64,15 +62,17 @@ public class RoomService implements IRoomService {
     @Override
     public EntityResult roomUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap) {
 
-        List<String>attrList = new ArrayList<>();
+        List<String> attrList = new ArrayList<>();
         attrList.add("id");
-        EntityResult result = roomQuery(attrMap, attrList);
-
+        EntityResult result = roomQuery(keyMap, attrList);
+        if (result.getMessage().contains("The room doesn't exists"))
+            return result;
         try {
             result = this.daoHelper.update(this.roomDao, attrMap, keyMap);
             result.setMessage("Successful room update");
-        }catch (Exception e){
-            if(e.getMessage().contains("Change the hotel of a room is not allowed"))
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            if (e.getMessage().contains("Change the hotel of a room is not allowed"))
                 result.setMessage("Change the hotel of a room is not allowed");
             else if (e.getMessage().contains("Number must be over zero"))
                 result.setMessage("Number must be over zero");

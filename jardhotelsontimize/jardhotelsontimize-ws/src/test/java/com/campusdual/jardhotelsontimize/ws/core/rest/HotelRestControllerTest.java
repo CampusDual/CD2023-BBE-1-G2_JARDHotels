0da132f9f652147e3573b6b29b6080fa5d9cdced
Lunantisidhe <rebeca.gonzalez.balado@gmail.com>
2023-06-05 14:05:57 +0200

    añadido test filter

---
+++ jardhotelsontimize/jardhotelsontimize-ws/src/test/java/com/campusdual/jardhotelsontimize/ws/core/rest/HotelRestControllerTest.java
@@ -0,0 +1,62 @@
+package com.campusdual.jardhotelsontimize.ws.core.rest;
+
+import com.campusdual.jardhotelsontimize.api.core.service.IHotelService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.dto.EntityResultMapImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class HotelRestControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private IHotelService hotelService;
+
+    @InjectMocks
+    private HotelRestController hotelRestController;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(hotelRestController).build();
+    }
+
+    @Test
+    public void testFilter() throws Exception {
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("columns", Arrays.asList("id", "name"));
+        requestBody.put("filter", new HashMap<>());
+
+        EntityResult entityResult = new EntityResultMapImpl();
+        entityResult.setCode(0);
+        entityResult.setMessage("The hotel has been found");
+        when(hotelService.hotelQuery(anyMap(), anyList())).thenReturn(entityResult);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/hotels/filter")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(requestBody)))
+                        .andReturn();
+
+        verify(hotelService, times(1)).hotelQuery(any(), any());
+        assertEquals(HttpStatus.OK.value(), mvcResult.getResponse().getStatus());
+    }
+}
